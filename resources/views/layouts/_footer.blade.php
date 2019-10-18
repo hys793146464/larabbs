ea1bff27f6ae0af738394337dae6d4ae9d698075
vagrant <vagrant@homestead>
2019-10-18 02:11:57 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由<a href="http://weibo.com/u/18742585931?is_hot=1" target="_blank">han</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:793146464@qq.com">联系我们</a></p>
+  </div>
+</footer>
